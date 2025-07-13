80348feb8634a9f8611d24ea8f24f8f0b262642b
Hammad <hammadasif0027@gmail.com>
2025-07-13 18:35:20 +0500

    feat: set up splash screen using the flutter_native_splash package

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Welcome to newsboard!'),
+      ),
+    );
+  }
+}
