763074beb60ef1d4215bc61e1ecf6a1e14cc9260
Hammad <hammadasif0027@gmail.com>
2025-07-13 20:09:20 +0500

    feat: set up light and dark theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'presentation/theme/app_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.system,
+      home: const HomeScreen(),
     );
   }
 }
---
+++ lib/presentation/theme/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get light => ThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: Colors.black,
+        colorScheme: const ColorScheme.light(
+          surface: Colors.white,
+          onSurface: Colors.black,
+          primary: Colors.black,
+          onPrimary: Colors.white,
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black),
+        ),
+      );
+
+  static ThemeData get dark => ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        primaryColor: Colors.white,
+        colorScheme: const ColorScheme.dark(
+          surface: Colors.black,
+          onSurface: Colors.white,
+          primary: Colors.white,
+          onPrimary: Colors.black,
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
+      );
+}
