bca7fe6ec5b592ae6cbb9a8e49b45e963d973ce9
Hammad <hammadasif0027@gmail.com>
2025-07-23 14:49:41 +0500

    feat(data): implement UserRepositoryImpl for user data operations
    - Add UserRepositoryImpl to handle user data persistence and retrieval
    - Integrate FirestoreUserDatasource and UserModel for data mapping

--- lib/data/repositories/user_repository_impl.dart
+++ lib/data/repositories/user_repository_impl.dart
@@ -1,1 +1,22 @@
+import '../../domain/entities/user.dart';
+import '../../domain/repositories/user_repository.dart';
+import '../datasources/firestore_user_datasource.dart';
+import '../models/user_model.dart';
 
+class UserRepositoryImpl implements UserRepository {
+  final FirestoreUserDatasource datasource;
+  UserRepositoryImpl(this.datasource);
+
+  @override
+  Future<void> saveUser({
+    required User user,
+  }) {
+    return datasource.saveUser(user: UserModel.fromEntity(user));
+  }
+
+  @override
+  Future<User?> getUser(String uid) async {
+    final userModel = await datasource.getUser(uid);
+    return userModel?.toEntity();
+  }
+}
