6a29b6e6de3b2e5ac37bc2e8c31c0f4651be45a1
Hammad <hammadasif0027@gmail.com>
2025-07-26 11:34:38 +0500

    feat(domain): add saved articles use cases and repository contract
    - Extend UserRepository contract to support saving, fetching, and removing saved articles.
    - Add use cases: SaveArticle, FetchSavedArticles, RemoveSavedArticle for user-saved news functionality.

--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -1,6 +1,11 @@
 import '../entities/user.dart';
+import '../entities/news.dart';
 
 abstract class UserRepository {
   Future<void> saveUser({required User user});
   Future<User?> getUser(String uid);
+  Future<void> saveArticle({required String uid, required News article});
+  Future<List<News>> fetchSavedArticles({required String uid});
+  Future<void> removeSavedArticle(
+      {required String uid, required String articleId});
 }
--- lib/domain/usecases/user_usecases.dart
+++ lib/domain/usecases/user_usecases.dart
@@ -1,5 +1,6 @@
 import 'package:newsboard/domain/repositories/user_repository.dart';
 
+import '../entities/news.dart';
 import '../entities/user.dart';
 
 class SaveUser {
@@ -19,5 +20,29 @@ class GetUser {
 
   Future<User?> call(String uid) {
     return repository.getUser(uid);
+  }
+}
+
+class SaveArticle {
+  final UserRepository repository;
+  SaveArticle(this.repository);
+  Future<void> call({required String uid, required News article}) {
+    return repository.saveArticle(uid: uid, article: article);
+  }
+}
+
+class FetchSavedArticles {
+  final UserRepository repository;
+  FetchSavedArticles(this.repository);
+  Future<List<News>> call({required String uid}) {
+    return repository.fetchSavedArticles(uid: uid);
+  }
+}
+
+class RemoveSavedArticle {
+  final UserRepository repository;
+  RemoveSavedArticle(this.repository);
+  Future<void> call({required String uid, required String articleId}) {
+    return repository.removeSavedArticle(uid: uid, articleId: articleId);
   }
 }
